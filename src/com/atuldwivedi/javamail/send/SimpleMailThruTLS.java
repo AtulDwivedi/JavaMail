42abdc12604325c2ce813d56ffbaf4745dcf7d4c
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-02-27 21:31:21 +0530

    Added example to send simple mail over SSL

--- src/com/atuldwivedi/javamail/send/SimpleMailThruTLS.java
+++ src/com/atuldwivedi/javamail/send/SimpleMailThruTLS.java
@@ -10,7 +10,7 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
-public class SimpleMail {
+public class SimpleMailThruTLS {
 
 	public static void main(String[] args) {
 

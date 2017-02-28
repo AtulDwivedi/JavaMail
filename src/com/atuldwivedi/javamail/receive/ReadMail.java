cb1c68509ae44da13102e17c8e3778bbd5be291b
Atul Dwivedi <er.dwivediatul@gmail.com>
2017-02-28 21:13:08 +0530

    Added example for receiving the emails

---
+++ src/com/atuldwivedi/javamail/receive/ReadMail.java
@@ -0,0 +1,65 @@
+package com.atuldwivedi.javamail.receive;
+
+import java.util.Locale;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.NoSuchProviderException;
+import javax.mail.Session;
+import javax.mail.Store;
+
+public class ReadMail {
+
+	public static void main(String[] args) {
+		ResourceBundle rb = ResourceBundle.getBundle("com//atuldwivedi//javamail//send//email", Locale.US);
+		String userName = rb.getString("username");
+		String password = rb.getString("password");
+
+		try{
+			//Get session object by providing properties field
+			Properties properties = new Properties();
+			properties.put("mail.pop3.host", "pop.gmail.com");
+			properties.put("mail.pop3.port", "995");
+			properties.put("mail.pop3.starttls.enable", "true");
+			Session emailSession =Session.getDefaultInstance(properties);
+
+			//Create the POP3 store object
+			Store store = emailSession.getStore("pop3s");
+
+			//Connect with the pop server
+			store.connect("pop.gmail.com", userName, password);
+
+			//Create the folder object
+			Folder emailFolder = store.getFolder("INBOX");
+
+			//Open the folder
+			emailFolder.open(Folder.READ_ONLY);
+
+			//Retrieve the messages from the folder in an array and print at most 10 messages
+			Message[] messages = emailFolder.getMessages();
+			System.out.println("messages.length---" + messages.length);
+
+			for (int i = 0, n = (messages.length > 10 ? 10:messages.length); i < n; i++) {
+				Message message = messages[i];
+				System.out.println("---------------------------------");
+				System.out.println("Email Number " + (i + 1));
+				System.out.println("Subject: " + message.getSubject());
+				System.out.println("From: " + message.getFrom()[0]);
+				System.out.println("Text: " + message.getContent().toString());
+			}
+			
+			//close the store and folder objects
+			emailFolder.close(false);
+			store.close();
+		} catch (NoSuchProviderException e) {
+			e.printStackTrace();
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
